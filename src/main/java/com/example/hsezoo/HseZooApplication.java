c3a79ad192abe02ace34949dad948b166c7c9d23
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2025-02-12 19:43:24 +0300

    initial comimt

---
+++ src/main/java/com/example/hsezoo/HseZooApplication.java
@@ -0,0 +1,13 @@
+package com.example.hsezoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HseZooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HseZooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hsezoo/HseZooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hsezoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HseZooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
